a2a307f4d93ecd3886023d5aade98dba9a971a8b
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2025-05-24 17:53:33 -0400

    100% page speed insights 😜

--- src/app.tsx
+++ src/app.tsx
@@ -2,13 +2,17 @@ import { jsxRenderer } from "hono/jsx-renderer";
 
 export default jsxRenderer(({ children }) => {
   return (
-    <html>
+    <html lang="en">
       <head>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="/styles.css" rel="stylesheet" />
         <link rel="icon" href="https://fav.farm/🕶️" />
         <title>letty</title>
+        <meta
+          name="description"
+          content="A typescript project template based on traditional web practices"
+        />
       </head>
       <body>
         {children}
