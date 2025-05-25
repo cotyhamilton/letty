a5eb45f6c178945e4c463afc17e51781d6ca0589
Coty Hamilton <13724650+cotyhamilton@users.noreply.github.com>
2025-05-25 17:31:22 -0400

    Use toast

--- src/components/hello-alert.tsx
+++ src/components/hello-alert.tsx
@@ -1,7 +1,7 @@
 export default function HelloAlert({ name }: { name: string }) {
   return (
-    <div class="fixed top-0 left-1/2 max-w-3xl">
-      <div class="alert floating-notification px-8 py-4 mt-4 bg-muted">
+    <div id="toaster" class="toaster">
+      <div class="toast alert floating-notification bg-muted">
         <h2 class="text-lg font-semibold">
           👋 hello, {name}!
         </h2>
@@ -10,19 +10,19 @@ export default function HelloAlert({ name }: { name: string }) {
         {`
           @keyframes slideInOut {
             0% {
-              transform: translate(-50%, -100%);
+              transform: translate(0%, 100%);
               opacity: 0;
             }
             15% {
-              transform: translate(-50%, 0);
+              transform: translate(0%, 0);
               opacity: 1;
             }
             85% {
-              transform: translate(-50%, 0);
+              transform: translate(0%, 0);
               opacity: 1;
             }
             100% {
-              transform: translate(-50%, -100%);
+              transform: translate(0%, 100%);
               opacity: 0;
             }
           }
